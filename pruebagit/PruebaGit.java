e18c85ec940f48cb8c5ccf895cc8fc13412d79f9
stiven <ico1293@gmail.com>
2016-12-26 00:12:34 -0500

    Agregado nuevo system out println para probar la actualizacion de version

--- pruebagit/PruebaGit.java
+++ pruebagit/PruebaGit.java
@@ -16,6 +16,7 @@ public class PruebaGit {
      */
     public static void main(String[] args) {
         System.out.println("hola la suma de 5 + 5 es : "+ (5+5));
+        System.out.println("agregando otra linea de texto :D para el push");
     }
     
 }
